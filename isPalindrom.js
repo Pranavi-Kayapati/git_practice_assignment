7b5ee27c5e03a1248c495c265c11512b5175374d
Pranavi-Kayapati <pranavikayapati01@gmail.com>
2022-12-04 23:43:44 +0530

    string is palindrome or not

---
+++ isPalindrom.js
@@ -0,0 +1,14 @@
+//this code is for finding whether the given string is palindrome or not.
+
+
+let str="masai";
+let newStr="";
+for(let i=str.length-1; i>=0; i++){
+newStr+=str[i];
+}
+if(str!==newStr){
+console.log(str,"is a palindrome");
+}
+else{
+console.log(str,"is not a palindrome");
+}
