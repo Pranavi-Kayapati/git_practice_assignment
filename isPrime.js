9a185dcc75ed3afd9e698d87c6f5ec03665f1efe
Pranavi-Kayapati <pranavikayapati01@gmail.com>
2022-12-04 23:33:39 +0530

    num is prime or not

---
+++ isPrime.js
@@ -0,0 +1,15 @@
+//this code is for finding whether the given number is prime or not.
+
+
+let num=10;
+let count=0;
+for(let i=1; i<=num; i++){
+if(num%i==0){
+count++;
+}
+if(count>2){
+console.log(num,"is prime number");
+}
+else{
+console.log(num,"is not a prime number");
+}
